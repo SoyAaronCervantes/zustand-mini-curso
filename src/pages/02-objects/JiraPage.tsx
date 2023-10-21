05804c6168f33f39f7f8b30c931b009eb8ef53a2
Aaron Cervantes <soy.aaron.cervantes@gmail.com>
2023-10-21 13:36:45 -0600

    feat: get task store, and split by type

--- src/pages/02-objects/JiraPage.tsx
+++ src/pages/02-objects/JiraPage.tsx
@@ -1,21 +1,25 @@
 import { JiraTasks } from '../../components';
+import {useTaskStore} from "../../stores";
 
 export const JiraPage = () => {
+  const openTasks = useTaskStore( state => state.getTasksByStatus('open'));
+  const doneTasks = useTaskStore( state => state.getTasksByStatus('done'));
+  const inProgressTasks = useTaskStore( state => state.getTasksByStatus('progress'));
   return (
     <>
       <h1>Tareas</h1>
-      <p>Manejo de estado con objectos de Zustand</p>
+      <p>Manejo de estado con objetos de Zustand</p>
       <hr />
 
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+      <aside className="grid grid-cols-1 md:grid-cols-3 gap-4">
           
-          <JiraTasks title='Pendientes' value='pending' />
+          <JiraTasks title='Pendientes' value='open' tasks={ openTasks } />
           
-          <JiraTasks title='Avanzando' value='in-progress' />
+          <JiraTasks title='Avanzando' value='progress' tasks={ inProgressTasks } />
           
-          <JiraTasks title='Terminadas' value='done' />
+          <JiraTasks title='Terminadas' value='done' tasks={ doneTasks } />
 
-      </div>
+      </aside>
 
       
 
